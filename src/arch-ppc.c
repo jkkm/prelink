d170078f762d551e0d61d3042d49ade64e713ab5
jakub <jakub@94c539fb-cf18-0410-b60f-edeeb537fa16>
2006-07-18 13:51:12 +0000

    20020919
    git-svn-id: svn://sourceware.org/svn/prelink@56 94c539fb-cf18-0410-b60f-edeeb537fa16

--- src/arch-ppc.c
+++ src/arch-ppc.c
@@ -424,6 +424,57 @@ ppc_arch_prelink (DSO *dso)
 }
 
 static int
+ppc_undo_prelink_rela (DSO *dso, GElf_Rela *rela, GElf_Addr relaaddr)
+{
+  switch (GELF_R_TYPE (rela->r_info))
+    {
+    case R_PPC_NONE:
+      return 0;
+    case R_PPC_RELATIVE:
+    case R_PPC_GLOB_DAT:
+    case R_PPC_ADDR32:
+    case R_PPC_UADDR32:
+    case R_PPC_REL32:
+      write_be32 (dso, rela->r_offset, 0);
+      break;
+    case R_PPC_JMP_SLOT:
+      /* .plt section will become SHT_NOBITS.  */
+      return 0;
+    case R_PPC_ADDR16:
+    case R_PPC_UADDR16:
+    case R_PPC_ADDR16_LO:
+    case R_PPC_ADDR16_HI:
+    case R_PPC_ADDR16_HA:
+      write_be16 (dso, rela->r_offset, 0);
+      break;
+    case R_PPC_ADDR24:
+    case R_PPC_REL24:
+      write_be32 (dso, rela->r_offset,
+		  read_ube32 (dso, rela->r_offset) & 0xfc000003);
+      break;
+    case R_PPC_ADDR14:
+      write_be32 (dso, rela->r_offset,
+		  read_ube32 (dso, rela->r_offset) & 0xffff0003);
+      break;
+    case R_PPC_ADDR14_BRTAKEN:
+    case R_PPC_ADDR14_BRNTAKEN:
+      write_be32 (dso, rela->r_offset,
+		  read_ube32 (dso, rela->r_offset) & 0xffdf0003);
+      break;
+    case R_PPC_COPY:
+      if (dso->ehdr.e_type == ET_EXEC)
+	/* COPY relocs are handled specially in generic code.  */
+	return 0;
+      error (0, 0, "%s: R_PPC_COPY reloc in shared library?", dso->filename);
+      return 1;
+    default:
+      error (0, 0, "%s: Unknown ppc relocation type %d", dso->filename,
+	     (int) GELF_R_TYPE (rela->r_info));
+      return 1;
+    }
+  return 0;
+}
+static int
 ppc_reloc_size (int reloc_type)
 {
   switch (reloc_type)
@@ -727,6 +778,7 @@ PL_ARCH = {
   .reloc_class = ppc_reloc_class,
   .max_reloc_size = 4,
   .arch_prelink = ppc_arch_prelink,
+  .undo_prelink_rela = ppc_undo_prelink_rela,
   .layout_libs_pre = ppc_layout_libs_pre,
   .layout_libs_post = ppc_layout_libs_post,
   /* This will need some changes in layout.c.
